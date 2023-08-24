960dba56175efce47e46267ec18f298408cc4699
MBELABED <Mustapha.13000@gmail.com>
2023-08-24 16:59:06 +0200

    Reorganizing project

--- src/main/java/com/Mustapha/SpringBootDemo/Models/PersonModel.java
+++ src/main/java/com/Mustapha/SpringBootDemo/Models/PersonModel.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Person {
+public class PersonModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
