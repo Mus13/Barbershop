b2fe6687d570e71d0dc5ada5528bd556be2c4d8c
MBELABED <Mustapha.13000@gmail.com>
2023-08-24 15:22:10 +0200

    Creating person entity

---
+++ src/main/java/com/Mustapha/SpringBootDemo/Models/Person.java
@@ -0,0 +1,42 @@
+package com.Mustapha.SpringBootDemo.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String firstName;
+    private String lastName;
+    private String Description;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
--- src/main/java/com/Mustapha/SpringBootDemo/SpringBootDemoApplication.java
+++ src/main/java/com/Mustapha/SpringBootDemo/SpringBootDemoApplication.java
@@ -3,7 +3,6 @@ package com.Mustapha.SpringBootDemo;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -15,7 +14,7 @@ public class SpringBootDemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	public CommandLineRunner commandLineRunner() {
 		return args -> {
 			System.out.println("Starting up !!");
 		};
